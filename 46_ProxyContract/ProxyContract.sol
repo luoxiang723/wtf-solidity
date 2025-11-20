387952ae1a139f730b17738b6ad74d4a80a1355d
luoxiang723 <515088980@qq.com>
2025-11-20 09:16:33 +0800

    proxycontract

---
+++ 46_ProxyContract/ProxyContract.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract Logic {
+    address implementation;
+    uint256 public x = 99;
+    event CallSuccess();
+
+    function increment() external returns (uint256) {
+        emit CallSuccess();
+        return x+1;
+    }
+}
+
+contract Proxy {
+    address implementation;
+
+    fallback() external payable {
+        _delegate();
+    }
+
+    constructor (address _implementation) {
+        implementation = _implementation;
+    }
+
+    function _delegate() internal {
+        assembly {
+            let _implementation := sload(0)
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), _implementation, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result 
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return (0, returndatasize())
+            }
+        }
+    }
+
+}
+
+contract Caller{
+    address proxy;
+    constructor(address _address){
+        proxy = _address;
+    }
+    function increse() external returns(uint) {
+        (, bytes memory data) = proxy.call(abi.encodeWithSignature("increment()"));
+        return abi.decode(data, (uint));
+    }
+}
